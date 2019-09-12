61d13659ab2a62a593be70c1e791ac3cfb0f8455
alkobunny <alkobunny@gmail.com>
2019-09-12 22:01:28 +0200

    Thursday

---
+++ scripts/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /root/sch1416/token.txt
+source /root/sch1416/scripts/date.sh
+
+TOKEN=$value
+
+echo $TOKEN
--- tokenUpdate.sh
+++ tokenUpdate.sh
@@ -2,7 +2,7 @@
 curl -O https://sch1416sv.mskobr.ru/form/pageDesc/250
 
 text='#!/bin/bash'
-fin='name="form[_token]"'
+fin='id="form__token"'
 mv 250 250.txt
 
 unset array
@@ -11,7 +11,7 @@ N=${#array[@]};
  
 for ((i = 1; i <= $N; i++)); do
 if [  "${array[$i]}" = "$fin" ]; then
-    token="${array1[$i+1]}"
+    token="${array[$i+2]}"
     echo -e "$text \n$token" > token.txt
 fi
 done 
